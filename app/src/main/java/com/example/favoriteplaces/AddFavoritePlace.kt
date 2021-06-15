3f422242962b948d139cd20bf5ce3fbd990bff77
OmerrKose <omer.kose@ibtech.com.tr>
2021-06-15 16:59:59 +0300

    Added date picker to the AddFavoritePage.

--- app/src/main/java/com/example/favoriteplaces/AddFavoritePlace.kt
+++ app/src/main/java/com/example/favoriteplaces/AddFavoritePlace.kt
@@ -1,10 +1,20 @@
 package com.example.favoriteplaces
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
 import androidx.appcompat.widget.Toolbar
+import java.text.SimpleDateFormat
+import java.util.*
 
-class AddFavoritePlace : AppCompatActivity() {
+class AddFavoritePlace : AppCompatActivity(), View.OnClickListener {
+
+    private val myFormat = "dd.MM.yyyy"
+    private var calendar = Calendar.getInstance()
+    private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_favorite_place)
@@ -15,5 +25,47 @@ class AddFavoritePlace : AppCompatActivity() {
         toolbar.setNavigationOnClickListener {
             onBackPressed()
         }
+
+        /*
+        * Set the date by clicking to the date input,
+        * after choosing the date update the input box accordingly
+        */
+        dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            calendar.set(Calendar.YEAR, year)
+            calendar.set(Calendar.MONTH, month)
+            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            updateDateInView()
+        }
+
+        findViewById<AppCompatEditText>(R.id.editTextDate).setOnClickListener(this)
+    }
+
+    /*
+    * When clicked open the date picker view and wait for the input
+    */
+    override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.editTextDate -> {
+                DatePickerDialog(
+                    this@AddFavoritePlace,
+                    dateSetListener,
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)
+                ).show()
+            }
+        }
+    }
+
+    /*
+    * When called update the date input box
+    * with the entered data from the data picker
+    * myFormat("dd.MM.yyyy")
+    */
+    private fun updateDateInView() {
+        val simpleDateFormat = SimpleDateFormat(myFormat, Locale.getDefault())
+        findViewById<AppCompatEditText>(R.id.editTextDate).setText(
+            simpleDateFormat.format(calendar.time).toString()
+        )
     }
 }
