c0d3faf0fc5759713ad2dacccf39f7b69eb35f8e
OmerrKose <omer.kose@ibtech.com.tr>
2021-06-17 16:28:52 +0300

    Can not access camera or gallery bug

--- app/src/main/java/com/example/favoriteplaces/AddFavoritePlace.kt
+++ app/src/main/java/com/example/favoriteplaces/AddFavoritePlace.kt
@@ -1,85 +1,85 @@
 package com.example.favoriteplaces
 
 import android.Manifest
-import android.app.AlertDialog
+import android.app.Activity
 import android.app.DatePickerDialog
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.provider.Settings
+import android.util.Log
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.appcompat.widget.AppCompatImageView
 import androidx.appcompat.widget.Toolbar
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.PermissionRequest
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
-import java.lang.Exception
+import java.io.IOException
 import java.text.SimpleDateFormat
 import java.util.*
 
+@Suppress("DEPRECATION")
 class AddFavoritePlace : AppCompatActivity(), View.OnClickListener {
 
-    private val myFormat = "dd.MM.yyyy"
-    private var calendar = Calendar.getInstance()
+    private var cal = Calendar.getInstance()
     private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_favorite_place)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbarAddPlace)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(findViewById(R.id.toolbarAddPlace))
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        toolbar.setNavigationOnClickListener {
+        findViewById<Toolbar>(R.id.toolbarAddPlace).setNavigationOnClickListener {
             onBackPressed()
         }
 
-        /*
-        * Set the date by clicking to the date input,
-        * after choosing the date update the input box accordingly
-        */
-        dateSetListener = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
-            calendar.set(Calendar.YEAR, year)
-            calendar.set(Calendar.MONTH, month)
-            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-            updateDateInView()
-        }
+        dateSetListener =
+            DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+                updateDateInView()
+            }
 
         findViewById<AppCompatEditText>(R.id.editTextDate).setOnClickListener(this)
         findViewById<TextView>(R.id.textViewAddImage).setOnClickListener(this)
     }
 
-    /*
-    * When clicked open the date picker view and wait for the input
-    */
     override fun onClick(v: View?) {
         when (v!!.id) {
             R.id.editTextDate -> {
                 DatePickerDialog(
                     this@AddFavoritePlace,
                     dateSetListener,
-                    calendar.get(Calendar.YEAR),
-                    calendar.get(Calendar.MONTH),
-                    calendar.get(Calendar.DAY_OF_MONTH)
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)
                 ).show()
             }
+
             R.id.textViewAddImage -> {
                 val pictureDialog = AlertDialog.Builder(this)
-                val pictureDialogItems = arrayOf("Select from Gallery", "Take from Camera")
                 pictureDialog.setTitle("Select Action")
-                pictureDialog.setItems(pictureDialogItems) { _, which ->
+                val pictureDialogItems =
+                    arrayOf("Select photo from gallery", "Capture photo from camera")
+                pictureDialog.setItems(
+                    pictureDialogItems
+                ) { _, which ->
                     when (which) {
                         0 -> choosePhotoFromGallery()
-                        1 -> Toast.makeText(
-                            this@AddFavoritePlace,
-                            "Camera selection coming...",
-                            Toast.LENGTH_LONG
-                        ).show()
+                        1 -> takePhotoFromCamera()
                     }
                 }
                 pictureDialog.show()
@@ -87,69 +87,142 @@ class AddFavoritePlace : AppCompatActivity(), View.OnClickListener {
         }
     }
 
-    /*
-    * This function is to allow user to pick an image from the photo gallery
-    * 2 types of permission is being asked, READ and WRITE,
-    * if permissions are not given by the user than direct to
-    * --> showRationaleDialogForPermission
-    */
+    /**
+     * Receive the result from a previous call to
+     * {@link #startActivityForResult(Intent, int)}.  This follows the
+     * related Activity API as described there in
+     * {@link Activity#onActivityResult(int, int, Intent)}.
+     *
+     * @param requestCode The integer request code originally supplied to
+     *                    startActivityForResult(), allowing you to identify who this
+     *                    result came from.
+     * @param resultCode The integer result code returned by the child activity
+     *                   through its setResult().
+     * @param data An Intent, which can return result data to the caller
+     *               (various data can be attached to Intent "extras").
+     */
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == GALLERY) {
+                if (data != null) {
+                    val contentURI = data.data
+                    try {
+                        val selectedImageBitmap =
+                            MediaStore.Images.Media.getBitmap(this.contentResolver, contentURI)
+
+                        findViewById<AppCompatImageView>(R.id.imageViewPlace)!!.setImageBitmap(
+                            selectedImageBitmap
+                        )
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                        Toast.makeText(this@AddFavoritePlace, "Failed!", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else if (requestCode == CAMERA) {
+                val thumbnail: Bitmap = data!!.extras!!.get("data") as Bitmap
+                findViewById<AppCompatImageView>(R.id.imageViewPlace)!!.setImageBitmap(thumbnail)
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            Log.e("Cancelled", "Cancelled")
+        }
+    }
+
+    /**
+     * A function to update the selected date in the UI with selected format.
+     * This function is created because every time we don't need to add format which we have added here to show it in the UI.
+     */
+    private fun updateDateInView() {
+        val myFormat = "dd.MM.yyyy"
+        val sdf = SimpleDateFormat(myFormat, Locale.getDefault())
+        findViewById<AppCompatEditText>(R.id.editTextDate).setText(
+            sdf.format(cal.time).toString()
+        )
+    }
+
+    /**
+     * A method is used for image selection from GALLERY / PHOTOS of phone storage.
+     */
     private fun choosePhotoFromGallery() {
-        Dexter.withActivity(this).withPermissions(
-            Manifest.permission.READ_EXTERNAL_STORAGE,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        ).withListener(object : MultiplePermissionsListener {
-            override fun onPermissionsChecked(report: MultiplePermissionsReport) {
-                if (report.areAllPermissionsGranted()) {
-                    Toast.makeText(
-                        this@AddFavoritePlace,
-                        "Storage permissions are granted. Please select an image from the gallery.",
-                        Toast.LENGTH_SHORT
-                    ).show()
+        Dexter.withActivity(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if (report!!.areAllPermissionsGranted()) {
+                        val galleryIntent = Intent(
+                            Intent.ACTION_PICK,
+                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                        )
+                        startActivityForResult(galleryIntent, GALLERY)
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+            }).onSameThread()
+            .check()
+    }
+
+    /**
+     * A method is used  asking the permission for camera and storage and image capturing and selection from Camera.
+     */
+    private fun takePhotoFromCamera() {
+        Dexter.withActivity(this)
+            .withPermissions(
+                Manifest.permission.READ_EXTERNAL_STORAGE,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.CAMERA
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                    if (report!!.areAllPermissionsGranted()) {
+                        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                        startActivityForResult(intent, CAMERA)
+                    }
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: MutableList<PermissionRequest>?,
+                    token: PermissionToken?
+                ) {
+                    showRationalDialogForPermissions()
+                }
+            }).onSameThread()
+            .check()
+    }
+
+    /**
+     * A function used to show the alert dialog when the permissions are denied and need to allow it from settings app info.
+     */
+    private fun showRationalDialogForPermissions() {
+        AlertDialog.Builder(this)
+            .setMessage("It Looks like you have turned off permissions required for this feature. It can be enabled under Application Settings")
+            .setPositiveButton(
+                "GO TO SETTINGS"
+            ) { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
                 }
             }
-
-            override fun onPermissionRationaleShouldBeShown(
-                permissions: MutableList<PermissionRequest>,
-                token: PermissionToken
-            ) {
-                showRationaleDialogForPermissions()
-            }
-        }).onSameThread().check()
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 
-    /*
-    * Function to display the user an alert box
-    * when rejected to give permission for the gallery
-    * direct user to the device settings
-    */
-    private fun showRationaleDialogForPermissions() {
-        AlertDialog.Builder(this).setMessage(
-            "You have rejected the permission." +
-                    "Please open it from the device settings in order to choose a photo from the gallery."
-        ).setPositiveButton("GO TO SETTINGS")
-        { _, _ ->
-            try {
-                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-                val uri = Uri.fromParts("package", packageName, null)
-                intent.data = uri
-                startActivity(intent)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
-        }.setNegativeButton("Cancel") { dialog, _ ->
-            dialog.dismiss()
-        }.show()
-    }
-
-    /*
-    * When called update the date input box
-    * with the entered data from the data picker
-    * myFormat("dd.MM.yyyy")
-    */
-    private fun updateDateInView() {
-        val simpleDateFormat = SimpleDateFormat(myFormat, Locale.getDefault())
-        findViewById<AppCompatEditText>(R.id.editTextDate).setText(
-            simpleDateFormat.format(calendar.time).toString()
-        )
+    companion object {
+        private const val GALLERY = 1
+        private const val CAMERA = 2
     }
 }
